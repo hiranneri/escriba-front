288f5ccaac7b7f6cca30130ef148b88debf05155
Hiran Neri <hiran.souza93@gmail.com>
2021-05-20 23:42:27 -0300

    Criando menu responsivo

--- src/components/shared/Menu.vue
+++ src/components/shared/Menu.vue
@@ -1,63 +1,130 @@
 <template>
     <header>
-        <div class="menu">
-            <router-link to="/" id="logoEscriba"> 
-                <img src="../../assets/img/escriba.png" alt="Logotipo Escriba" id="imgEscriba" >
-            </router-link>
-            <nav>
-                <ul>
-                    <li v-for="rota in rotas" :key="rota.name">
-                        <router-link :to="rota.path ? rota.path : '/'" class="item"> 
-                            <p>{{ rota.name }}</p>
-                        </router-link>
-                    </li>
-                </ul>
-            </nav>
-        </div>   
+        <router-link to="/" id="logoEscriba"> 
+            <img src="../../assets/img/escriba.png" alt="Logotipo Escriba" id="imgEscriba" >
+        </router-link>
+
+        <nav class="navegacao">
+            <button v-on:click="toogleMenu" v-on:touchstart="toogleMenu" class="botao-mobile">Menu
+                <span class="icone-hamburger"></span>
+            </button>
+
+            <ul class="menu">
+                <li v-for="rota in rotas" :key="rota.name">
+                    <router-link :to="rota.path ? rota.path : '/'" class="item-menu"> 
+                        <p>{{ rota.name }}</p>
+                    </router-link>
+                </li>
+            </ul>
+        </nav>        
     </header>
 </template>
 <script>
 export default {
     name:'Menu',
-    props:['rotas']
+    props:['rotas'],
+    methods:{
+        toogleMenu(event){
+            if(event==='touchstart') event.preventDefault();
+            let menuNav = document.querySelector('.navegacao');
+            menuNav.classList.toggle('active')
+        }
+    }
 
 }
 </script>
 <style scoped>
     header{
+        box-sizing: border-box;
+        display: flex;
         background-color: black;
-        padding: 20px 0;
         font-family: 'Roboto', sans-serif;
         font-size: 20px;
         font-weight: bold;
+        align-items: center;
+        height: 70px;
+        padding: 1rem;
+        justify-content: space-between;
+        padding-top: 40px;
+        padding-bottom: 50px ;
+    }
+    .item-menu{
+        text-decoration: none;
+        color: white; 
+        display: block;
+        padding: .5rem;
+
+    }
+    .item-menu:hover{
+        color: yellow;
     }
     .menu{
-       
-        width: 940px;
-        position: relative;
-        margin: 0 auto;
-    }
-    nav{
-        position: absolute;
-        top:20px;
-        right: 0;         
-    }
-    li{
+        display: flex;
         list-style: none;
-        color: white;
-        text-decoration: none;
+        gap: .8rem;
     }
-    p{
-        text-transform: uppercase;
-        color: white;
+    .botao-mobile{
+        display: none;
     }
-    .item{
-        display: inline-block;
-        text-decoration: none;
-        margin-left: 50px;
-    }
-    p:hover{
-        color: yellow;
+
+    @media (max-width: 600px){
+        .botao-mobile{
+            display: flex;
+            padding: .5rem .1rem;
+            font-size: 1rem;
+            border:none;
+            background: none;
+            cursor: pointer;
+            gap: .5rem;
+            color: yellow;
+        }
+        .icone-hamburger{
+            color: yellow;
+            border-top: 2px solid;
+            width: 20px;
+        }
+        .menu{
+            display: block;
+            position: absolute;
+            width: 100%;
+            top: 70px;
+            right: 0;
+            background: black;
+            height: 0;
+            z-index: 1000;
+            transition: .3s;
+            visibility: hidden;
+            overflow-y: auto;
+        }
+        .navegacao.active .menu{ 
+            height: 70vh;
+            visibility: visible;
+        }
+        .item-menu{
+            padding: 1rem 0;
+            margin-left: 0 1rem;
+            border-bottom: 2px solid rgba(235, 235, 141, 0.681);
+        }
+        .icone-hamburger::after, .icone-hamburger::before{
+            content: '';
+            display: block;
+            width: 20px;
+            height: 2px;
+            margin-top: 5px;
+            background: currentColor;
+            transition: .3s;
+            position: relative;
+        }
+        .navegacao.active .icone-hamburger{
+            border-top-color: transparent;   
+        }
+        nav.active .icone-hamburger::before{
+            transform: rotate(135deg);
+        }
+        nav.active .icone-hamburger::after{
+            transform: rotate(-135deg);
+            top: -7px;
+        }
     }
 
 </style>
